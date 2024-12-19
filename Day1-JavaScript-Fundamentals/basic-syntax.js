03d7f4a3b25003a8f83f45d3fe70f98e3f6073c0
Devansh Sharma <devanshsh25@gmail.com>
2024-12-19 23:04:30 +0530

    Added JS Fundamentals

---
+++ Day1-JavaScript-Fundamentals/basic-syntax.js
@@ -0,0 +1,4 @@
+// Understanding Scope
+/* Scope determines the accessibility of variables, objects, and functions in different
+parts of code. It defines where variables can be declared, accessed and modified. 
+*/
