a9566b77c33153ffc0765725e092e6929d0761f1
Devansh Sharma <devanshsh25@gmail.com>
2024-12-20 22:49:05 +0530

    Scope in JavaScript

--- Day1-JavaScript-Fundamentals/basic-syntax.js
+++ Day1-JavaScript-Fundamentals/basic-syntax.js
@@ -2,3 +2,14 @@
 /* Scope determines the accessibility of variables, objects, and functions in different
 parts of code. It defines where variables can be declared, accessed and modified. 
 */
+/* Global Scope */
+/* Variables declared outside any function or block have global scope and can be accessed from anywhere in the code. */
+
+let globalVar = "I am a global variable";
+
+function printGlobal() {
+  console.log(globalVar); // accessible here
+}
+
+printGlobal();
+console.log(globalVar); // accessible here
