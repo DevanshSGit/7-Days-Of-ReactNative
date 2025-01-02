818c96140e4b543f89b44acf70cf4f32abfdda4f
Devansh Sharma <devanshsh25@gmail.com>
2025-01-02 23:21:33 +0530

    Arrow Functions

--- Day1-JavaScript-Fundamentals/functions.js
+++ Day1-JavaScript-Fundamentals/functions.js
@@ -13,3 +13,18 @@ const multiply = function (a, b) {
   return a * b;
 };
 console.log(multiply(2, 3));
+
+// Arrow Functions
+// Not hoisted
+
+const subtract = (a, b) => a - b;
+
+console.log(subtract(4, 3));
+
+// Multi-line arrow function
+const divide = (a, b) => {
+  if (b === 0) return "Cannot divide by 0";
+  return a / b;
+};
+
+console.log(divide(10 / 2));
