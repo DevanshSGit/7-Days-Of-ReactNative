fda5adc1bad7a804ef78218d4b029cf6d4ab34ad
Devansh Sharma <devanshsh25@gmail.com>
2025-01-02 23:17:21 +0530

    Funcion Expressions

--- Day1-JavaScript-Fundamentals/functions.js
+++ Day1-JavaScript-Fundamentals/functions.js
@@ -5,3 +5,11 @@ function add(a, b) {
   return a + b;
 }
 console.log(add(2, 3));
+
+// Function Expressions
+// Not hoisted
+
+const multiply = function (a, b) {
+  return a * b;
+};
+console.log(multiply(2, 3));
